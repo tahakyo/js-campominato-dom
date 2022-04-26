5baa18a60278008ca8656ee06020392c539879ce
tahakyo <ghazouani1988@email.com>
2022-04-26 23:20:44 +0200

    add function to generate an array for bombs

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@
 // Generare una griglia di gioco quadrata in cui ogni cella contiene un numero compreso tra 1 e 100.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
+// consegna ++ 
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 // Al click sul bottone Play far partire il gioco
 const playButton = document.getElementById("play-btn");
@@ -17,6 +23,7 @@ function startGame() {
     title.classList.add("hidden");
     grid.classList.remove("hidden");
     grid.innerHTML = "";
+    
     // Prelevare la scelta della difficoltà dell'utente
     const difficulty = parseInt(document.getElementById('level').value);
     let cellNumber = 0;
@@ -31,6 +38,11 @@ function startGame() {
         cellNumber = 49;
         cellNumberInRow = 7;
     }
+    // generare 16 bombe: 16 numeri casulai compres tra 1 e cellNumber
+        const bombsNumber = 16;
+        const bombsArray = generateUniqueRndmNumbers (bombsNumber, cellNumber);
+        console.log(bombsArray);
+
     // generare le celle da 1 a 100
     for (let i = 1; i <= cellNumber; i++) {
         const newItem = generateGridItem(i, cellNumberInRow);
@@ -61,4 +73,25 @@ function generateGridItem(gridNumber, cellsInRow) {
     return gridItem;
 }
 
+// UTILITY FUNCTIONS
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
+/**
+ * Description : la funzione genera numeri random non ripetuti
+ *
+ * @param {Number} numberQuantity --> la quantità dei numeri da generare
+ * @param {Number} maxLimit --> il limite massimo di range dei numeri
+ * @returns {Array} --> array dei numeri random non ripetuti
+ */
+function generateUniqueRndmNumbers (numberQuantity, maxLimit) {
+    const numbersArray = [];
+    while (numbersArray.length < numberQuantity) {
+        const rndmNumbers = getRndInteger (1, maxLimit);
+        if (!numbersArray.includes(rndmNumbers)) {
+            numbersArray.push(rndmNumbers);
+        }
+    }
+    return numbersArray;
+}
